299aea7f8346fbc34487dee87151950a9483354e
MatejMa2ur <matejmazur@outlook.com>
2024-02-19 22:50:58 +0100

    Initial Commit

---
+++ app/appApp.swift
@@ -0,0 +1,17 @@
+//
+//  appApp.swift
+//  app
+//
+//  Created by Matej Mazur on 19/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
